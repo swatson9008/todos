aa90ac5c41e333f5b0a71c2367df2169cf673310
Summer <swatson9008@yahoo.com>
2022-12-15 11:00:34 -0500

    got the import working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("test");
+
+
+let submitB = document.getElementById("submitB");
+let TDTitle = document.getElementById("TDTitle");
+let TDDescrip = document.getElementById("TDDescrip");
+let TDDueDate = document.getElementById("TDDueDate");
+let TDPriority = document.getElementById("TDPriority");
+
+let indexCounter = 0;
+
+const masterList = new Map();
+
+
+import { createNewToDo } from "./createtodo";
+
+class toDoCreate {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+const secondExample = new toDoCreate ("Sweep Floors", "Clean floors", "Tomorrow", "Low");
+
+submitB.addEventListener("click", e => createNewToDo(e));
+
+
+
