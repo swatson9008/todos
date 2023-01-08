77028bb9f6fe29bf74def1d50a0701212a09cb76
Summer <swatson9008@yahoo.com>
2023-01-08 11:47:22 -0500

    changed config

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,21 @@ module.exports = {
   },
   mode: 'development',
   devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
