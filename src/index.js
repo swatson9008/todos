0c09f285a502743b260b220e49ee4245dd581d36
Summer <swatson9008@yahoo.com>
2023-01-08 15:48:10 -0500

    adjusted css

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ import { displayHomeTab } from './displayControl';
 import { displayProjectTab } from './displayControl';
 import { displayTaskList } from './displayControl';
 import { displayProjectLists } from './displayControl';
-import './style.css';
 
 const submitB = document.getElementById('submitB');
 const TDTitle = document.getElementById('TDTitle');
